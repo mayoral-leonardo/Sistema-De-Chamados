0755b6446e91781b53a9e48eb968442c3215bfac
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-01-23 23:40:09 -0300

    created login page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,9 +1,34 @@
-import react from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './SignIn.css';
+import logo from '../../assets/images/logo.png';
 
-export default function SignIn () {
+export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Clicou')
+  }
+
   return (
-    <div>
-      <h1>Página de Login</h1>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='logo-area'>
+          <img src={logo} alt='Logo do Sistema' />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type='text' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type='password' placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link to='/register'>Criar uma conta</Link>
+
+      </div>
     </div>
   )
 }
