ab9a8c7222f05681d7512532b83f233c22c7a40e
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-01-26 21:40:19 -0300

    created sidebar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import './Header.css';
+import { AuthContext } from '../../contexts/auth';
+import avatar from '../../assets/images/avatar.png';
+import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi';
+
+export default function Header() {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className='sidebar'>
+      <div>
+        <img src={user.avatarUrl === null || user.avatarUrl === undefined ? avatar : user.avatarUrl} alt='Imagem do usuário' />
+      </div>
+      <Link to='/dashboard'>
+      <FiHome color='#FFFFFF' size={24}/>
+        Chamados
+      </Link>
+      <Link to='/dashboard'>
+      <FiUser color='#FFFFFF' size={24}/>
+        Clientes
+      </Link>
+      <Link to='/dashboard'>
+      <FiSettings color='#FFFFFF' size={24}/>
+        Configurações
+      </Link>
+    </div>
+  )
+
+}
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
+import Header from "../../components/Header/Header";
 
 export default function Dashboard () {
   const { signOut } = useContext(AuthContext);
   return (
     <div>
+      <Header/>
       <h1>Dashboard</h1>
       <button onClick={() => signOut() }>Logout</button>
     </div>
