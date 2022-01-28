aa107e7ca207ee4f647950a4fa30fdf75c1f4e06
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-01-27 22:02:44 -0300

    finished profile page update functions

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -4,14 +4,94 @@ import Header from '../../components/Header/Header';
 import Title from '../../components/Title/Title';
 import avatar from '../../assets/images/avatar.png'
 import { FiSettings, FiUpload } from 'react-icons/fi';
+import firebase from '../../services/firebaseConnection';
 import { AuthContext } from '../../contexts/auth';
+import { toast } from "react-toastify";
 
 export default function Profile() {
-  const { user, signOut } = useContext(AuthContext);
+  const { user, signOut, setUser, storageUser } = useContext(AuthContext);
 
   const [name, setName] = useState(user && user.name);
   const [email, setEmail] = useState(user && user.email);
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  function handleFile(e) {
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
+
+      if (image.type === 'image/jpeg' || image.type === 'image/png') {
+        setImageAvatar(image);
+        setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+      } else {
+        alert('Envie uma imagem do tipo JPEG ou PNG');
+        setImageAvatar(null);
+        return null;
+      }
+    }
+  }
+
+
+  async function handleUpload() {
+    const currentUid = user.uid;
+    const uploadTask = await firebase.storage()
+      .ref(`images/${currentUid}/${imageAvatar.name}`)
+      .put(imageAvatar)
+      .then(async () => {
+        console.log('FOTO ENVIADA COM SUCESSO!');
+
+        await firebase.storage().ref(`images/${currentUid}`)
+          .child(imageAvatar.name).getDownloadURL()
+          .then(async (url) => {
+            let urlFoto = url;
+
+            await firebase.firestore().collection('users')  
+              .doc(user.uid)
+              .update({
+                avatarUrl: urlFoto,
+                name: name
+              })
+              .then(() => {
+                let data = {
+                  ...user,
+                  avatarUrl: urlFoto,
+                  name: name
+                };
+                setUser(data);
+                storageUser(data);
+              })
+
+          })
+      })
+  }
+
+  async function handleSave(e) {
+    e.preventDefault();
+
+    if (imageAvatar === null && name !== '') {
+      await firebase.firestore().collection('users')
+        .doc(user.uid)
+        .update({
+          name: name
+        })
+        .then(() => {
+          let data = {
+            ...user,
+            name: name
+          }
+          setUser(data);
+          storageUser(data);
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error('Ops... algo deu errado !');
+        })
+    }
+
+    else if (name !== '' && imageAvatar !== null) {
+      handleUpload()
+    }
+  }
 
   return (
     <div>
@@ -22,13 +102,13 @@ export default function Profile() {
         </Title>
 
         <div className='container'>
-          <form className='form-profile'>
+          <form className='form-profile' onSubmit={handleSave}>
             <label className='label-avatar'>
               <span><FiUpload color='#FFFFFF' size={25} /></span>
-              <input type='file' accept='image/*'></input>
+              <input type='file' accept='image/*' onChange={handleFile}></input>
               {avatarUrl === null ?
                 <img src={avatar} width='250' alt='Foto de perfil do usuário' />
-                : <img src={avatar} width='250' alt='Foto de perfil do usuário' />
+                : <img src={avatarUrl} width='250' alt='Foto de perfil do usuário' />
               }
             </label>
 
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDb--8I2kqqC4EkGj36quaOiqgTYu0j5-g",
