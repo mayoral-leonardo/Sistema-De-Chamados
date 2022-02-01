19c73748ea6df7d5e1b598055fc14b45fcb58500
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2022-01-31 23:33:58 -0300

    created tickets table

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import './Dashboard.css';
 import { useState } from "react";
 import Header from '../../components/Header/Header';
 import Title from '../../components/Title/Title';
-import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
@@ -29,9 +29,40 @@ export default function Dashboard() {
               <FiPlus size={25} color='#FFFFFF' />
               Novo Chamado
             </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope='col'>Cliente</th>
+                  <th scope='col'>Assunto</th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Cadastrado em</th>
+                  <th scope='col'>#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label='Cliente'>Teste Cliente</td>
+                  <td data-label='Assunto'>Teste Assunto</td>
+                  <td data-label='Status'>
+                    <span className='badge' style={{ backgroundColor: '#5CB85C' }}>Teste Status</span>
+                  </td>
+                  <td data-label='Cadastrado'>Teste Cadastrado</td>
+                  <td data-label='#'>
+                    <button className='action' style={{ backgroundColor: '#3583F6' }}>
+                      <FiSearch color='#FFFFFF' size={17}/>
+                    </button>
+                    <button className='action' style={{ backgroundColor: '#F6A935' }}>
+                      <FiEdit2 color='#FFFFFF' size={17}/>
+                    </button>
+                  </td>
+
+
+                </tr>
+              </tbody>
+            </table>
           </>
         )}
-
 
       </div>
     </div>
